2617ea643c0c33a657bc187f0e852d2fddece5e2
Adam Gardner <adam.gardner4991@gmail.com>
2020-06-26 21:44:30 +0100

    initial state, axios and store files created

---
+++ frontend-ui-five-aside/src/data/initial.js
@@ -0,0 +1,11 @@
+const initial = {
+    players: [],
+    team_one_name: "Team One",
+    team_two_name: "Team Two",
+    team_one_colour: "navy",
+    team_two_colour: "darkred",
+    history: [],
+    loaded: false
+}
+
+export default initial;
