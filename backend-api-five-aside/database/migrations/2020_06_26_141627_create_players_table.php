ef12e9571c66c794df1616623fbe49dc72a271a7
Adam Gardner <adam.gardner4991@gmail.com>
2020-06-26 15:19:55 +0100

    Player table created

---
+++ backend-api-five-aside/database/migrations/2020_06_26_141627_create_players_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->string("player_name", 30);
+            $table->integer("skill");
+            $table->integer("team")->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
