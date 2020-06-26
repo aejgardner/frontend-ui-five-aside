3ba737f257678dd66e7320973dfa2e4d3055923a
Adam Gardner <adam.gardner4991@gmail.com>
2020-06-26 16:53:32 +0100

    Index method set up in Players controller, fillable properties set up in Player model

--- backend-api-five-aside/app/Player.php
+++ backend-api-five-aside/app/Player.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Player extends Model
 {
-    //
+    // Only allow the player_name and player_rating fields to be updated via mass assignment
+    protected $fillable = ["player_name", "player_rating"];
 }
