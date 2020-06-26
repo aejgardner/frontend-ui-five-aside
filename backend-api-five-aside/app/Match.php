8a751cd9be92ffa9c290d8a9a4abb870627c9b56
Adam Gardner <adam.gardner4991@gmail.com>
2020-06-26 18:10:31 +0100

    Fillable properties added to Match model, casts variable also added, so that JSON data for each team's players can be converted back to arrays

--- backend-api-five-aside/app/Match.php
+++ backend-api-five-aside/app/Match.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Match extends Model
 {
-    //
+    // only allow these fields to get updated via mass assignment
+    protected $fillable = ["team_one_name", "team_one_score", "team_two_name", "team_two_score", "team_one_players", "team_two_players"];
+
+    // converts the JSON stored in the matches table back into two arrays
+    protected $casts = [
+        "team_one_players" => 'array',
+        "team_two_players" => 'array',
+    ];
 }
