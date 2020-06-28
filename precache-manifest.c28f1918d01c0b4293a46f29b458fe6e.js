12f9b3b09da16560b9e18f7e2f9b6feb184da05c
Adam Gardner <adam.gardner4991@gmail.com>
2020-06-28 14:42:37 +0100

    Updates

---
+++ precache-manifest.c28f1918d01c0b4293a46f29b458fe6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047f5371382722c77c7b3ca00dc7080e",
+    "url": "/frontend-ui-five-aside/index.html"
+  },
+  {
+    "revision": "c86479082d43231d7bb7",
+    "url": "/frontend-ui-five-aside/static/css/main.af73c009.chunk.css"
+  },
+  {
+    "revision": "1b4de36f7278ca3e9f86",
+    "url": "/frontend-ui-five-aside/static/js/2.9ba2b8f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontend-ui-five-aside/static/js/2.9ba2b8f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c86479082d43231d7bb7",
+    "url": "/frontend-ui-five-aside/static/js/main.26b402f8.chunk.js"
+  },
+  {
+    "revision": "6e30f11afcc216dc4aa2",
+    "url": "/frontend-ui-five-aside/static/js/runtime-main.4a214453.js"
+  },
+  {
+    "revision": "d32682d2f8bee2733e5019eb83c69519",
+    "url": "/frontend-ui-five-aside/static/media/stadium.d32682d2.jpg"
+  }
+]);
